113137a285fdff97f884ff16e2c9ee05bf8a030a
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2024-01-12 13:06:12 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Button from "@mui/material/Button";
+
 import "./App.css";
 import MainTextArea from "./comp/textArea.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 function App() {
   return (
     <div className="container">
-      <MainTextArea />
-      <Button className="my-4" variant="contained">
-        UPPERCASE
-      </Button>
+      <MainTextArea heading="Enter text below" />
     </div>
   );
 }
--- src/comp/textArea.jsx
+++ src/comp/textArea.jsx
@@ -1,31 +1,54 @@
 
 
 
-
-
-import { Flex, Input } from 'antd';
-
+import Button from "@mui/material/Button";
+import { useState } from 'react';
+import { Input } from 'antd';
 
 
 const { TextArea } = Input;
-const onChange = (e) => {
-  console.log('Change:', e.target.value);
-};
 
-const MainTextArea = () => {
+const MainTextArea = (p) => {
+  const [text, setText] = useState("")
+  const clickbtn = () => {
+    let newText = text.toUpperCase()
+    setText(newText)
+
+  }
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const clearAll = ()=>{
+    setText('')
+  }
   return (
-    <div>
-      <TextArea
-        showCount
-        maxLength={100}
-        onChange={onChange}
-        placeholder="disable resize"
-        style={{
-          height: 120,
-          resize: "none",
-        }}
-      />
-    </div>
+    <>
+      <div>
+        <h1>{p.heading}</h1>
+        <TextArea
+          value={text}
+          maxLength={100}
+          onChange={onChange}
+          placeholder="disable resize"
+          style={{
+            height: 120,
+            resize: "none",
+          }}
+        />
+        <Button onClick={clickbtn} className="my-4" variant="contained">
+          UPPERCASE
+        </Button>
+        <Button onClick={clearAll} className="m-5" variant="contained">
+          Clear All
+        </Button>
+      </div>
+      <div className="container">
+        <h1>Your Text summery</h1>
+        <p>{text.split(" ").length - 1 } : Words</p>
+        <br />
+        <p>{text.length} : Characters</p>
+      </div>
+    </>
   );
 };
 
